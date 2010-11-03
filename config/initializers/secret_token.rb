8787fd99abc90a66549a923c0ffce64ec71a8ec5
opsb <oliver@opsb.co.uk>
2010-11-03 14:33:46 +0000

    Added rails and cucumber dependencies

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CukeTest::Application.config.secret_token = 'df989f9e9095f579302d4f3e6fe9cc5aa02d55d3b0444e9a64a2366d3b07cddc7c39f315d658c96515ec6b618a2b3f613c213ea9becd9efde7f87cc9a297b95e'
